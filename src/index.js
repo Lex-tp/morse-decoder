fbbc03d5ccaa97792bcf464906c6a1c4d2e57d57
Denis Korotkevich <volk.denis98@gmail.com>
2020-03-13 10:26:35 +0300

    feat: Morse code decoder done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result='';
+    let subStr='';
+    let morseStr='';
+    let decryptStr='';
+    for(let i=10; i<=expr.length;i+=10){
+        subStr=expr.substring(i-10,i);
+        for(let j=2;j<=subStr.length;j+=2){
+            morseStr=subStr.substring(j-2,j);
+            if(morseStr=='10'){
+                result+='.';
+            }else if(morseStr=='11'){
+                result+='-';
+            }else if(morseStr=='**'){
+                result+='**'
+            }else{
+                result='';
+            }
+        }
+        decryptStr+=comparison(result);
+        result='';
+    }
+    return decryptStr;
+}
+
+function comparison (str) {
+    for (var key in MORSE_TABLE) {
+        if(str=='**********'){
+            return ' ';
+        }
+        if(key==str){
+            return MORSE_TABLE[key];
+        }
+    }
 }
 
 module.exports = {
